11f87cb575830c356f68d34af9f0d230616601db
EvgenSakun <evgen.sakun20@gmail.com>
2024-01-28 19:44:56 +0300

    start

---
+++ HM18/HM18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM18
+//
+//  Created by Evgeny Sakun on 28.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
